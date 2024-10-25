0ab32b18535978021df28eda7cee3b5f60e8c3b6
Twinkle Hingu <64422394+Twinklehingu@users.noreply.github.com>
2024-10-24 21:04:41 -0700

    Add files via upload

---
+++ Data/VillaStore.cs
@@ -0,0 +1,12 @@
+﻿using WebAPIDemos.Models.Dto;
+
+namespace WebAPIDemos.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO>() { 
+             new VillaDTO{ Id=1, Name="Pool View", Sqft=500, Occupancy=5},
+             new VillaDTO{ Id=2, Name="Beach View", Sqft = 600, Occupancy=6}
+        };
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using WebAPIDemos.Data;
+using WebAPIDemos.Logging;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+//Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
+//    .WriteTo.File("log/villaLogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+
+//builder.Host.UseSerilog();
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DeafultSqlConnection"));
+});
+
+builder.Services.AddControllers(option => {
+    //option.ReturnHttpNotAcceptable = true;
+}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ILogging, LoggingV2>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
